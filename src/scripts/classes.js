19d33e4bd54444371a3df5d9a6ca0b18db84d1c7
Egori <skornyakovegor231199@gmai.,com>
2023-04-18 16:48:34 +0500

    first version

---
+++ src/scripts/classes.js
@@ -0,0 +1,13 @@
+class Task{
+    constructor() {
+        console.log("Создфт экземпляр Task!")
+    }
+
+    showId(){
+        console.log(23)
+    }
+
+    static loadAll(){
+        console.log("Загружаем все tasks...")
+    }
+}
